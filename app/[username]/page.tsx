889e59ecca633b8e03497338b39e0c4634a387a7
Sean Coker <1030830+okcoker@users.noreply.github.com>
2025-01-09 17:01:22 -0500

    Update

--- app/[username]/page.tsx
+++ app/[username]/page.tsx
@@ -8,7 +8,7 @@ async function Page({ params }: PageProps) {
   const { username } = await params;
 
   return (
-    <div className="size-full px-2 pt-4 sm:px-10 sm:pt-10">
+    <div className="size-full px-2 pt-4 sm:px-10 sm:pt-10 bg-red-500">
       <p>User page for: {username}</p>
       <Link href="/photo/12">Open modal</Link>
     </div>
