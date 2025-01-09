3b8775a09e72d4a441f0eca12f369f6a04d62b1b
Sean Coker <1030830+okcoker@users.noreply.github.com>
2025-01-09 18:29:49 -0500

    Style buttons

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,27 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1>Intercepted routes bug</h1>
+        <h1 className="font-bold text-lg">Intercepted route bugs</h1>
 
-        <Link href="/example1" className="hover:opacity-50">
+        <Link
+          href="/example1"
+          className="bg-gray-800 hover:bg-gray-700 rounded-md p-3"
+        >
           Example 1: Link to intercepted route with one level up marker (..)
           marker <br />
           ❌ dev <br />❌ vercel
         </Link>
-        <Link href="/example2" className="hover:opacity-50">
+        <Link
+          href="/example2"
+          className="bg-gray-800 hover:bg-gray-700 rounded-md p-3"
+        >
           Example 2: Link to intercepted route with root (...) marker <br />
           ❌ dev <br />❌ vercel
         </Link>
-        <Link href="/my-username" className="hover:opacity-50">
+        <Link
+          href="/my-username"
+          className="bg-gray-800 hover:bg-gray-700 rounded-md p-3"
+        >
           Example 3: Link to intercepted route with root (...) marker inside
           dynamic segment
           <br />
