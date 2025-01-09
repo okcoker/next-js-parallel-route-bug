860de077d4b6d0daa0c9982631bcacda6b38a1f8
Sean Coker <1030830+okcoker@users.noreply.github.com>
2025-01-09 18:41:19 -0500

    Remove console log

--- app/example3/@modal/(...)photo/[id]/page.tsx
+++ app/example3/@modal/(...)photo/[id]/page.tsx
@@ -3,7 +3,6 @@ type PageProps = {
 };
 
 export default function Page({}: PageProps) {
-  console.log("page.tsx:5", "here");
   return (
     <div className="bg-white size-20 text-black text-center p-4">
       Hello darkness, my old friend.
